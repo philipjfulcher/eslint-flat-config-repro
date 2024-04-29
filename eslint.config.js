03a8b8961e7e4a318478b0889d1395f2ff395a38
Philip Fulcher <philip@nrwl.io>
2024-04-29 11:33:14 -0600

    removin empty rules property

--- eslint.config.js
+++ eslint.config.js
@@ -30,16 +30,13 @@ module.exports = [
   ...compat.config({ extends: ['plugin:@nx/typescript'] }).map((config) => ({
     ...config,
     files: ['**/*.ts', '**/*.tsx'],
-    rules: {},
   })),
   ...compat.config({ extends: ['plugin:@nx/javascript'] }).map((config) => ({
     ...config,
     files: ['**/*.js', '**/*.jsx'],
-    rules: {},
   })),
   ...compat.config({ env: { jest: true } }).map((config) => ({
     ...config,
     files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
-    rules: {},
   })),
 ];
